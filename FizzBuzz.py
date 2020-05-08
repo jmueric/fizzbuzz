8490ddf1aeb7b9c48287b6c50a0fadfcfe4b1763
jmueric <59981920+jmueric@users.noreply.github.com>
2020-05-07 20:00:48 -0400

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,13 @@
+i = 0
+
+
+while i < 101:
+    if i % 15 == 0:
+        print("FizzBuzz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    else:
+        print(i)
+    i += 1
